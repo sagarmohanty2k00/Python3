af2abe5bb4a2c3e063539030c531b21683a6059d
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2022-01-17 23:19:12 +0530

    Blockchain class created

---
+++ BlockChain Dev/blockchain/blockchain/blockchain.py
@@ -0,0 +1,16 @@
+class BlockChain(object):
+    def __init__(self):
+        self.chain = []
+
+    def new_block(self):
+        # Generates a new block and adds it to the chain
+        pass
+
+    @staticmethod
+    def hash(self):
+        # Hashes a block
+        pass
+
+    def last_block(self):
+        # Gets the latest block in the chain
+        pass
